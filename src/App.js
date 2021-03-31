bea9b1ab97f5cb826f88034d8c489b02fe2193f8
kaizzz2001 <nguyenngocvietthinh@gmail.com>
2021-03-31 15:27:47 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.scss';
+
+function App() {
+  return (
+    //class name thuong khong de chu hoa
+    <div className="app"> 
+      <h1>Welcome to React Hooks</h1>
+    </div>
+  );
+}
+
+export default App;
